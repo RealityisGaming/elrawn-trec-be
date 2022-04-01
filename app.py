2e2d1b8b46f9d65a4c65f4ce558bdff2c87828f9
Matthew Hoecker <98118482+RealityisGaming@users.noreply.github.com>
2022-04-01 18:01:19 -0500

    'Please'

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 import psycopg2
 import os
-
+# sad
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
